e4c1fe90c3ce1162712c6bf05a6698846e55af78
Alex46 <WorkProject46@outlook.com>
2021-12-28 11:23:13 +0200

    add routing /home /about /privacy

--- www/form-content.php
+++ www/form-content.php
@@ -3,10 +3,9 @@ function setHeader($color='black'){
     echo "<header style=\"border: 2px solid $color; height: 8vh;\"> HEADER </header>";
 }
 function setNav($color="black"){
-    echo "
-        <nav style=\"border: 2px solid $color; height: 7vh;\">
+    echo "<nav style =\"border: 2px solid $color; height: 7vh;\">
             <div class='navigateDiv'>
-                <a href='main.php'><button >Home</button></a>
+                <a href='index.php'><button >Home</button></a>
                 <a href='about.php'><button >About</button></a>
                 <a href='privacy.php'><button>Privacy</button></a>
             </div>
@@ -18,6 +17,7 @@ function setDiv($color="black"){
 function setAside($color="black"){
     echo "<aside style=\"border: 2px solid $color; display: flex; height: 75vh; flex-grow: 2\">ASIDE</aside>";
 }
-function setFooter($color="black"){
-    echo "<footer style=\"border: 2px solid $color; height: 10vh;\">FOOTER</footer>";
+function setFooter($color="black")
+{
+    echo "<footer style=\"border: 2px solid $color; height: 8vh;\">FOOTER</footer>";
 }
--- www/index.php
+++ www/index.php
@@ -1,20 +1,38 @@
+<html lang="es">
+<style>
+    *{margin: 0;padding: 0;}
+     .navigateDiv{
+        width: 100%;
+        height: 100%;
+        background: #ccccee;
+        display: flex;
+        align-items: center;
+    }
+    .navigateDiv > a > button{
+        border: 1px solid whitesmoke;
+        height: 80%;
+        padding: 0 10px 0 10px;
+        width: 100px;
+        font-size: 14pt;
+        margin-left: 5px;
+        border-radius: 5px;
+    }
+</style>
 <?php
-//print_r ($_SERVER);
-//phpinfo();
-$x = 10;
-@include "functions.php";
-if ( ! function_exists ("print_hello")){
-    echo "<i>Runtime error </i>";
-    exit;
-}
+    include_once 'form-content.php';
+    //print_r ($_SERVER);
+    //phpinfo();
+    setHeader("red");
+    setNav();
+?>
+<div style="display: flex; flex-direction: row; flex-wrap: nowrap;">
+<?php
+    setDiv("orange");
+    setAside();
+?>
+</div>
+<?php
+    setFooter("green");
+?>
+</html>
 
-function print_x(){
-    global $x;
-    echo $x;
-}
-
-$f = function(){
-    return "Hello";
-};
-
-include_once "view.php";
